e56f1669c194f4019f326e20100b2530235094d6
DaianaHLK <130119321+DaianaHLK@users.noreply.github.com>
2025-03-16 13:52:15 -0500

    retirando funcion de focus

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []; // Lista para almacenar los nombres
-document.getElementById("amigo").focus();
 
 function agregarAmigo() {
     let input = document.getElementById("amigo");
